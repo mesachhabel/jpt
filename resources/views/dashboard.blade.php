23403bf652fcae992da67c5daf02626414c99998
Bayu Rezky Ramadhan <bayu221200@gmail.com>
2022-03-24 13:20:31 +0700

    dashboard update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,41 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Hello World!</h1>
+<div class="container-xxl flex-grow-1 container-p-y">
+<div class="col-lg-8 mb-4 order-0">
+    <div class="card">
+        <div class="d-flex align-items-end row">
+            <div class="col-sm-7">
+                <div class="card-body">
+                    <h5 class="card-title text-primary">Selamat datang Nama Karyawan!</h5>
+                        <p class="mb-4">
+                            Silahkan cek informasi terkait data karyawan anda.
+                        </p>
+                </div>
+            </div>
+            <div class="col-sm-5 text-center text-sm-left">
+                <div class="card-body pb-0 px-0 px-md-4">
+                    <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140" alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="col-lg-6 col-md-12 col-6 mb-4">
+<div class="card">
+    <div class="card-body">
+        <div class="card-title d-flex align-items-start justify-content-between">
+            <div class="avatar flex-shrink-0">
+                <img src="../assets/img/icons/unicons/chart-success.png" alt="chart success" class="rounded"/>
+            </div>
+        </div>
+        <span class="fw-semibold d-block mb-1">Total Karyawan</span>
+        <h3 class="card-title mb-2">17</h3>
+    </div>
+</div>
+</div>
+</div>
 @endsection

9d6fedf14ef626693022c859bf701d4931f0e574
Bayu Rezky Ramadhan <bayu221200@gmail.com>
2022-04-01 10:37:37 +0700

    update js

--- public/assets/js/datakaryawan.js
+++ public/assets/js/datakaryawan.js
@@ -12,3 +12,17 @@ selectElement.addEventListener('change', (event) => {
 function statuskar(value) {
     document.getElementById("tb").disabled = value != "2";
 }
+
+function imagePreview(fileInput) {
+    if (fileInput.files && fileInput.files[0]) {
+        var fileReader = new FileReader();
+        fileReader.onload = function(event) {
+            $('#uploadedAvatar').html('<img src="' + event.target.result + '" width="100" height="100"/>');
+        };
+        fileReader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+$("#inputImage").change(function() {
+    imagePreview(this);
+});

920532e7042859b6916a2ae749ef0d23616fcc82
Bayu Rezky Ramadhan <bayu221200@gmail.com>
2022-04-01 16:46:04 +0700

    update tr penandatanganan

--- resources/views/admins/TabelReferensi/TR/Penandatangan.blade.php
+++ resources/views/admins/TabelReferensi/TR/Penandatangan.blade.php
@@ -14,6 +14,8 @@
             </button>
 
             <!-- Modal -->
+            <form id="formAccountSettings" method="POST" action="{{ route('penandatangan.store') }}">
+                @csrf
             <div class="modal fade" id="modalCenter" tabindex="-1" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
@@ -25,19 +27,19 @@
                             <div class="row">
                                 <div class="col mb-0">
                                     <label for="dobWithTitle" class="form-label">NIK</label>
-                                    <input type="text" id="dobWithTitle" class="form-control" placeholder="Kode Sub-Unit" />
+                                    <input type="text" id="dobWithTitle" class="form-control" placeholder="Kode Sub-Unit" name="nik"/>
                                 </div>
                             </div>
                             <div class="row mt-2">
                                 <div class="col mb-0">
                                     <label for="emailWithTitle" class="form-label">Nama</label>
-                                    <input type="text" id="emailWithTitle" class="form-control" placeholder="Keterangan Unit" />
+                                    <input type="text" id="emailWithTitle" class="form-control" placeholder="Keterangan Unit" name="nama"/>
                                 </div>
                             </div>
                             <div class="row mt-2">
                                 <div class="col mb-0">
                                     <label for="dobWithTitle" class="form-label">Jabatan</label>
-                                    <input type="text" id="dobWithTitle" class="form-control" placeholder="Keterangan Sub-Unit" />
+                                    <input type="text" id="dobWithTitle" class="form-control" placeholder="Keterangan Sub-Unit" name="jabatan"/>
                                 </div>
                             </div>
                         </div>
@@ -45,7 +47,7 @@
                             <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
                                 Close
                             </button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
@@ -58,30 +60,31 @@
         <div class="table-responsive text-nowrap">
             <table class="table table-hover">
                 <thead>
-                    <tr>
-                        <th>No.</th>
+                    <tr>     
                         <th>NIK</th>
                         <th>Nama</th>
                         <th>Jabatan</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
+                @foreach ($penandatangans as $ttd)
                 <tbody class="table-border-bottom-0">
                     <tr>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>000</strong></td>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>1</strong></td>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Kantor Pusat</strong></td>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Kantor Pusat</strong></td>
+                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>{{$ttd->nik}}</strong></td>
+                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>{{$ttd->nama}}</strong></td>
+                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>{{$ttd->jabatan}}</strong></td>
                         <td>
                             <a href="{{url('view-skalagaji')}}" type="button" class="btn btn-sm btn-secondary"><i class="bx bx-file"></i></a>
                             <a href="{{url('edit-skalagaji')}}" type="button" class="btn btn-sm btn-success"><i class="bx bx-edit"></i></a>
                             <a href="{{url('delete-skalagaji')}}" type="button" class="btn btn-sm btn-danger"><i class="bx bx-trash"></i></a>
                         </td>
                     </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </div>
+    <ul class="pagination justify-content-center mt-3">{{ $penandatangans->links('pagination::bootstrap-4') }} </ul>
     <!--/ Hoverable Table rows -->
 </div>
 <hr class="my-5" />

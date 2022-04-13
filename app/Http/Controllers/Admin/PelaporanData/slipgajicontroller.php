d471ab95f84995347906ea5f7aa40884307b66cb
Bayu Rezky Ramadhan <bayu221200@gmail.com>
2022-04-13 11:44:39 +0700

    slip gaji

--- app/Http/Controllers/Admin/PelaporanData/slipgajicontroller.php
+++ app/Http/Controllers/Admin/PelaporanData/slipgajicontroller.php
@@ -10,10 +10,82 @@ class slipgajicontroller extends Controller
 {
     public function index()
     {
+        return view ('admins.PelaporanData.SlipGaji.SlipGaji');
+    }
+
+    public function slipkomisaris()
+    {
+        return view ('admins.PelaporanData.SlipGaji.SG.SlipGajiKomisaris');
+    }
+    
+    public function slipdireksi()
+    {
+        return view ('admins.PelaporanData.SlipGaji.SG.SlipGajiDireksi');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tr_agama $agama)
+    {
         
-        // $pdf = PDF::loadview('admins/PelaporanData/SlipGaji');  
-        
-        // return $pdf->stream('SlipGaji.pdf');
-        return view ('admins/PelaporanData/SlipGaji');
     }
 }
---
+++ resources/views/admins/PelaporanData/SlipGaji/SlipGaji.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.main')
+
+@section('container')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4">
+            <span class="text-muted fw-light">Pelaporan Data /</span> Slip Gaji
+        </h4>
+
+        <!-- Hoverable Table rows -->
+        <div class="card">
+            <div class="table-responsive text-nowrap">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Nama Slip Gaji</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <tr>
+                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Slip Gaji Komisaris</strong></td>
+                            <td>
+                                <a href="{{ route('slipgaji.slipkomisaris') }}" type="button" 
+                                    class="btn btn-sm btn-success"><i class="bx bx-edit"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Slip Gaji Direksi</strong>
+                            </td>
+                            <td>
+                                <a href="{{ route('slipgaji.slipdireksi') }}" type="button"
+                                    class="btn btn-sm btn-success"><i class="bx bx-edit"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Slip Gaji Karyawan Tetap</strong>
+                            </td>
+                            <td>
+                                <a href="{{ route('kodestruktur.index') }}" type="button"
+                                    class="btn btn-sm btn-success"><i class="bx bx-edit"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Slip Gaji Karyawan Tidak Tetap</strong>
+                            </td>
+                            <td>
+                                <a href="{{ route('kodejabatan.index') }}" type="button" class="btn btn-sm btn-success"><i
+                                        class="bx bx-edit"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <hr class="my-5" />
+@endsection

ed62dfa3984a4317ddb5329f545a7f85c6c3261b
Bayu Rezky Ramadhan <bayu221200@gmail.com>
2022-04-28 09:09:37 +0700

    perbaikan margin

--- resources/views/admins/PelaporanData/SlipGaji/SG/CetakSlipGajiDireksi.blade.php
+++ resources/views/admins/PelaporanData/SlipGaji/SG/CetakSlipGajiDireksi.blade.php
@@ -257,14 +257,14 @@
                                     <div class="separ wraptext" id="bloc2">
                                         <a>:<br>:<br>:<br>:<br>:</a>
                                     </div>
-                                    <div class="keterangan wraptext" id="bloc3">
+                                    <div class="keterangan wraptext" id="bloc3" style="width:auto;">
                                         <a>{{ old('nik', $direksi->nik) }} - {{ old('nama', $direksi->nama) }} <br>
                                             {{ old('jabatan', $direksi->jabatan) }}<br>
                                             {{ old('npwp', $direksi->npwp) }} <br>
                                             {{ old('tmk', $direksi->tmk) }}
                                             <br>{{ $beetween }} </a>
                                     </div>
-                                    <div class="status" id="bloc4">
+                                    <div class="status" id="bloc4" style="margin-left:35px;">
                                         <a>Status Karyawan <br> Status Keluarga</a>
                                     </div>
                                     <div class="separ ii" id="bloc5">

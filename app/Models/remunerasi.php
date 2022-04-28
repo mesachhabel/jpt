c365cc910024865d4963329cb49cefb3a41b4f53
Bayu Rezky Ramadhan <bayu221200@gmail.com>
2022-04-28 10:04:54 +0700

    x

--- app/Models/remunerasi.php
+++ app/Models/remunerasi.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\tr_kodejabatan;
 
 class remunerasi extends Model
 {
     use HasFactory;
     protected $guarded = [];
+    
 }
